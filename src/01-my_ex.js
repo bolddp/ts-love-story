66f0fd8ab4ababd554a1d76685d9f59039fb8710
Daniel <daniel.p.persson@husqvarnagroup.com>
2019-09-09 17:50:17 +0200

    work in progress

--- src/01-my_ex.js
+++ src/01-my_ex.js
@@ -6,14 +6,3 @@ class CustomerService {
     console.log(`Customer inserted (id: ${customer.id})`);
   }
 }
-
-const srv = new CustomerService();
-
-// Oops, missing id...
-srv.insert({ name: 'customer01' });
-
-// Hmm, what type is id?
-srv.insert({ id: 123, name: 'customer01' }); // Works just fine
-srv.insert({ id: 'abc', name: 'customer01' }); // Also works
-
-
---
+++ src/07-3rd_party.ts
@@ -0,0 +1,1 @@
+// 3rd party and module support
